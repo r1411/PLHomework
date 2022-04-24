7f1eaf1c507b0bdc88e07ce147588c9f15880791
r1411 <roma00055@gmail.com>
2022-04-24 23:06:05 +0300

    Задача 12.11 (рек. вниз)

--- Lab12/Lab12.pl
+++ Lab12/Lab12.pl
@@ -7,3 +7,8 @@ is_prime(K, X) :- K1 is K+1, is_prime(K1,X). % Проверяем дальше
 snd(X, Result) :- snd(X, X, Result).
 snd(_, 2, 1) :- !.
 snd(X, CurDel, Result) :- NewDel is CurDel - 1, snd(X, NewDel, R1), Ost is X mod CurDel, (Ost = 0, not(is_prime(CurDel)), Result is R1 + CurDel; Result is R1), !.
+
+% 11. Найти сумму непростых делителей числа (рек. вниз)
+snd_d(X, Result) :- snd_d(X, X, 1, Result).
+snd_d(_, 2, Result, Result) :- !.
+snd_d(X, CurDel, CurSum, Result) :- NewDel is CurDel - 1, Ost is X mod CurDel, (Ost = 0, not(is_prime(CurDel)), NewSum is CurSum + CurDel; NewSum is CurSum), snd_d(X, NewDel, NewSum, Result), !.
