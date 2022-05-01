62ac1ca3a8cc0a754bb414c2448b227b3480757e
r1411 <roma00055@gmail.com>
2022-05-01 15:04:09 +0300

    Задача 13.20

--- Lab13/Lab13.pl
+++ Lab13/Lab13.pl
@@ -185,3 +185,46 @@ task19 :-
     in_list(Friends,[AuFriend, australia, _]),
     in_list(Friends,[richard, _, RSport]),
     write('Australian: '), write(AuFriend), nl, write('Richard plays: '), write(RSport), !.
+
+%%% 20 (Вар. 6) [Имя, Предмет]
+
+% Пусть чем ближе к началу списка человек, тем он моложе
+
+task20 :-
+    Teachers = [_, _, _],
+    in_list(Teachers, [morozov, _, _]),
+    in_list(Teachers, [vasilev, _, _]),
+    in_list(Teachers, [tokarev, _, _]),
+
+    (in_list(Teachers, [_, history, _]); in_list(Teachers, [_, _, history])),
+    (in_list(Teachers, [_, math, _]); in_list(Teachers, [_, _, math])),
+    (in_list(Teachers, [_, biology, _]); in_list(Teachers, [_, _, biology])),
+    (in_list(Teachers, [_, geography, _]); in_list(Teachers, [_, _, geography])),
+    (in_list(Teachers, [_, english, _]); in_list(Teachers, [_, _, english])),
+    (in_list(Teachers, [_, french, _]); in_list(Teachers, [_, _, french])),
+
+    not(in_list(Teachers, [_, geography, french])),
+    not(in_list(Teachers, [_, french, geography])),
+
+    not(in_list(Teachers, [_, biology, math])),
+    not(in_list(Teachers, [_, math, biology])),
+
+    after([_, math, _], [_, biology, _], Teachers),
+    after([morozov, _, _], [vasilev, _, _], Teachers),
+    after([morozov, _, _], [tokarev, _, _], Teachers),
+
+    not(in_list(Teachers, [tokarev, biology, _])),
+    not(in_list(Teachers, [tokarev, _, biology])),
+    not(in_list(Teachers, [tokarev, french, _])),
+    not(in_list(Teachers, [tokarev, _, french])),
+    not(in_list(Teachers, [_, biology, french])),
+    not(in_list(Teachers, [_, french, biology])),
+
+    not(in_list(Teachers, [morozov, math, _])),
+    not(in_list(Teachers, [morozov, _, math])),
+    not(in_list(Teachers, [morozov, english, _])),
+    not(in_list(Teachers, [morozov, _, english])),
+    not(in_list(Teachers, [_, math, english])),
+    not(in_list(Teachers, [_, english, math])),
+
+    write(Teachers), !.
