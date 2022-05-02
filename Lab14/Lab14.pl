f653c4583002b38ed46fd54c76c8ab3a3643eeb9
r1411 <roma00055@gmail.com>
2022-05-02 16:29:38 +0300

    Задача 14.1.2

--- Lab14/Lab14.pl
+++ Lab14/Lab14.pl
@@ -10,3 +10,11 @@ write_str([H|Tail]):-put(H),write_str(Tail).
 %%% Задание 1
 % 1.1
 task1_1 :- read_str(Str, Len), write_str(Str), write(", "), write_str(Str), write(", "), write_str(Str), write(" "), write(Len).
+
+% 1.2
+count_symbols(Str, Search, Result) :- char_code(Search, SCode), count_symbols(Str, SCode, 0, Result).
+count_symbols([], _, Result, Result) :- !.
+count_symbols([S|T], Search, CurCnt, Result) :- S = Search, NewCnt is CurCnt + 1, count_symbols(T, Search, NewCnt, Result), !.
+count_symbols([_|T], Search, CurCnt, Result) :- count_symbols(T, Search, CurCnt, Result), !.
+
+task1_2 :- read_str(Str, _), count_symbols(Str, " ", SpacesCnt), WordsCnt is SpacesCnt + 1, write("Words count: "), write(WordsCnt).
